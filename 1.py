1f4b5fc0412ea5230853159f5aeb6213b741cd83
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-10-31 13:38:09 +0200

    Completed challenge 1

---
+++ 1.py
@@ -0,0 +1,13 @@
+from time import sleep
+f = open("folder/input1.txt","r")
+file = f.readlines()
+length = len(file)
+num1 = int(file[0].strip())
+increaseCount = 0
+for i in range(1,length):
+    num2 = int(file[i].strip())
+    if num2 > num1:
+        increaseCount += 1
+    num1 = num2
+print("The total increaseCount is:", increaseCount)
+f.close()
